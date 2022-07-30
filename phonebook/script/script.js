d7c5857fca93205589f7de402ad0e32bb802b632
iiiivanCh <iiiivanchm@yandex.ru>
2022-07-30 03:40:34 +0300

    повторил то что было в видео

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -193,6 +193,7 @@ const data = [
       logo,
       btnAdd: buttonGroup.btns[0],
       formOverlay: form.overlay,
+      form: form.form,
     };
   };
 
@@ -245,7 +246,7 @@ const data = [
     const app = document.querySelector(selectorApp);
     const phoneBook = renderPhoneBook(app, title);
 
-    const { list, logo, btnAdd, formOverlay } = phoneBook;
+    const { list, logo, btnAdd, formOverlay, form } = phoneBook;
 
     //Функционал
 
@@ -253,13 +254,17 @@ const data = [
 
     hoverRow(allRow, logo);
 
-    const objEvent = {
-      handleEvent() {
-        formOverlay.classList.add('is-visible');
-      },
-    };
+    btnAdd.addEventListener('click', () => {
+      formOverlay.classList.add('is-visible');
+    });
 
-    btnAdd.addEventListener('click', objEvent);
+    form.addEventListener('click', event => {
+      event.stopPropagation();
+    });
+
+    formOverlay.addEventListener('click', () => {
+      formOverlay.classList.remove('is-visible');
+    });
   };
 
   window.phoneBookInit = init;
